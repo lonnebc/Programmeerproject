785453c3011b8437d5614f48ca19f99fea637819
lonnebc <lonneke.lammers@hotmail.com>
2016-01-06 14:16:27 +0100

    Gosia check

--- future_employment/csvtojson.py
+++ future_employment/csvtojson.py
@@ -1,17 +1,23 @@
 import json
+import csv
 
-with open("Employment-Projections.csv","rb") as lines:
-	lines = lines.readlines()[1:119]
-    
-
-data = []
-
-for line in lines:
-	#splitted_title = line.split('           ')
-	#title = splitted_title[0]
-    splitted = line.split(',')
-    wage = splitted[-7]
+with open("datacsv.csv","rb") as fp:
+	#lines = fp.readlines()[1:119]
+	#lines = fp.readlines()
+	#print type(lines[0])
+	#print lines[0]
+	reader = csv.reader(fp,',')
+	print reader
+# data = []
+# for line in lines:
+# 	print line[0]
+# 	# new_line = line.replace("          ",",")
+	# #print line.split()
+	# splitted = line.split(",")
+	# title = splitted[1]
+	#splitted = line.split(',')
+	#wage = splitted[-7]
     #years8 = splitted[-1:-8].strip()
-    data.append([wage]) 
-json_data = json.dumps(data)
-print json_data
+# 	data.append([title]) 
+# json_data = json.dumps(data)
+# print json_data
